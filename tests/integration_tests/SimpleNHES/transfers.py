11fece094343688684a6719e8af52034eefb16ba
Prism <andrewtrainor20@gmail.com>
2021-01-04 16:11:25 -0700

    added stuff

---
+++ tests/integration_tests/SimpleNHES/transfers.py
@@ -0,0 +1,46 @@
+
+# Copyright 2020, Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+"""
+  Implements transfer functions
+"""
+
+def electric_consume(data, meta):
+  ## works with generic?
+  # activity = meta['raven_vars']['HERON_pyomo_model']
+  # t = meta['t']
+  # flip sign because we consume the electricity
+  # E = -1.0 * activity['electricity'][t]
+
+  ## works with pyomo
+  # model = meta['HERON']['pyomo_model']
+  # component = meta['HERON']['component']
+  activity = meta['HERON']['activity']
+  # TODO a get_activity method for the dispatcher -> returns object-safe activity (expression or value)?
+  amount = -1 * activity['electricity']
+  data = {'driver': amount}
+  return data, meta
+
+def generator(data, meta):
+  effciency = 0.7 # Just a random guess at a turbine efficiency
+
+  if 'steam' in data:
+    # Determine the electricity output for a given steam input
+    data['electricity'] = -1 * effciency * data['steam']
+  elif 'electricity' in data:
+    # Determine the steam input for a given electricity output
+    data['steam'] = -1/effciency * data['electricity']
+  else:
+    raise Exception("Generator Transfer Function: Neither 'electricity' nor 'steam' given")
+
+  return data, meta
+
+
+def flex_price(data, meta):
+  sine = meta['HERON']['RAVEN_vars']['Signal']
+  t = meta['HERON']['time_index']
+  # DispatchManager
+  # scale electricity consumed to flex between -1 and 1
+  amount = - 2 * (sine[t] - 0.5)
+  data = {'reference_price': amount}
+  return data, meta
